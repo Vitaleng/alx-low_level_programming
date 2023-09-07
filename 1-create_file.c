412d4b9ce4d4b4ee27444d35caa01e275792c694
Vitaleng <frecklesineeleng@gmail.com>
2023-09-07 02:58:25 -0700

    C - File I/O

--- 1-create_file.c
+++ 1-create_file.c
@@ -10,26 +10,21 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd;
-	int nletters;
-	int rwr;
-
-	if (!filename)
+	int fd, w, len = 0;
+	
+	if (!filename == NULL)
 		return (-1);
 
-	fd = open(filename, 0_CREAT | 0_WRONLY | 0_TRUNC, 0600);
+	if (text_content!= NULL)
+	{
+		for (len=0; text_content[len];)
+			len++;
+	}
 
-	if (fd == -1)
-		return (-1);
+	fd = oprn(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	w = write(fd, text_content, len);
 
-	if (!text_content)
-		text_content = "";
-
-	for (nletters = 0; text_content[nletters]; nletters++)
-		;
-	rwr = write(fd, text_content, nletters);
-
-	if (rwr == -1)
+	if (fd == -1 || w  == -1)
 		return (-1);
 
 	close(fd);
